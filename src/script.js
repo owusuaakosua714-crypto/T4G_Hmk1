f811007b6ad2f9a0d630383672a37f1dc993119b
Akosua <owusuaakosua714@gmail.com>
2026-07-20 13:05:24 +0000

    Added script.js to src

---
+++ src/script.js
@@ -0,0 +1,7 @@
+// Prompt the user for their details
+let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+let email = prompt("Enter your email:");
+
+// Display the formatted message
+alert(`Hi ${name}, you are ${age} years old and we'll keep in touch via your email: ${email}`);
